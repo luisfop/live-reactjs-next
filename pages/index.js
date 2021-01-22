833900ead0ae69cfae61b31bb5e7742c172d5786
luisop <luisfop77@gmail.com>
2021-01-22 19:31:03 -0300

    commit test

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Bem vindo a Imersão React-NextJs
+          Baby, love u me 2, merci ?
         </h1>
 
         <p className={styles.description}>
